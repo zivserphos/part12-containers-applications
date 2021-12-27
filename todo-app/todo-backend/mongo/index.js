1d6ccbf9c52c0551015e6704a8cd47af2de7699e
zivserphos <zivfromisrael@gmail.com>
2021-12-27 20:20:24 +0200

    12.7

--- todo-app/todo-backend/mongo/index.js
+++ todo-app/todo-backend/mongo/index.js
@@ -1,10 +1,15 @@
-const mongoose = require('mongoose')
-const Todo = require('./models/Todo')
-const { MONGO_URL } = require('../util/config')
+const mongoose = require("mongoose");
+const Todo = require("./models/Todo");
+const { MONGO_URL } = require("../util/config");
 
-if (MONGO_URL && !mongoose.connection.readyState) mongoose.connect(MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+console.log(MONGO_URL);
 
+if (MONGO_URL && !mongoose.connection.readyState)
+  mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 
 module.exports = {
-  Todo
-}
+  Todo,
+};
